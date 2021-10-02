1651b97523cc7541b1cb3e748a3d621c6c460584
Namainya <wekesa350@gmail.com>
2021-10-02 13:02:07 +0300

    commit on pop

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp( 
       title: 'Flutter Demo',
       theme: myTheme,
       home: const Home(),
